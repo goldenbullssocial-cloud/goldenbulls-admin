8f7c69c8b8977dc495a50ea927ead957b737da61
mansi <mansi365.rejoice@gmail.com>
2026-02-24 18:15:26 +0530

    fix:  on cancel button added state

--- src/components/outlineButton/index.js
+++ src/components/outlineButton/index.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import styles from './outlineButton.module.scss';
-import classNames from 'classnames';
-export default function OutlineButton({ text, className, icon }) {
-    return (
-        <div className={classNames(styles.outlineButton, className, icon ? styles.buttonIconAlignment : "")}>
-            <button aria-label={text}>
-                <span>
-                    {text}
-                </span>
-                {
-                    icon && (
-                        <img src={icon} altl={icon} />
-                    )
-                }
-            </button>
-        </div>
-    )
+import React from "react";
+import styles from "./outlineButton.module.scss";
+import classNames from "classnames";
+export default function OutlineButton({ text, className, icon, onClick }) {
+  return (
+    <div
+      className={classNames(
+        styles.outlineButton,
+        className,
+        icon ? styles.buttonIconAlignment : "",
+      )}
+    >
+      <button aria-label={text} onClick={onClick}>
+        <span>{text}</span>
+        {icon && <img src={icon} alt={icon} />}
+      </button>
+    </div>
+  );
 }
